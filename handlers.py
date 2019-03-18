c7d2630faf1b9cb4f2f763e1d5c33b5c510862c0
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-03-18 22:16:32 +0000

    logging done

--- handlers.py
+++ handlers.py
@@ -1,5 +1,7 @@
 from typing import Optional, Awaitable, Generator
 import threading
+from datetime import datetime
+from datetime import timedelta
 
 import tornado.web
 import docker
@@ -9,6 +11,7 @@ from docker.models.containers import Container
 
 from stream_logs import stream_logs_from_generator
 
+RFC_3339_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 DOCKER_CLIENT: DockerClient = docker.from_env()
 
 
@@ -24,10 +27,13 @@ class GeneralHTTPHandler(tornado.web.RequestHandler):
 class StartLoggingHandler(GeneralHTTPHandler):
     def get(self, name: str):
         try:
+            since_raw = self.get_argument("since", None)
+            since = (datetime.strptime(since_raw[0:26] + since_raw[29], RFC_3339_FORMAT) + timedelta(
+                milliseconds=1) if since_raw else None)
             container: Container = DOCKER_CLIENT.containers.get(name)
-            generator: Generator = container.logs(stream=True, timestamps=True)
+            generator: Generator = container.logs(stream=True, timestamps=True, since=since)
             if len(list(filter(lambda t: t.name == name, threading.enumerate()))) == 0:
-                threading.Thread(target=stream_logs_from_generator, args=(name, generator), name=name,
+                threading.Thread(target=stream_logs_from_generator, args=(name, generator, since_raw), name=name,
                                  daemon=True).start()
         except NotFound:
             pass
--- stream_logs.py
+++ stream_logs.py
@@ -6,15 +6,18 @@ import asyncio
 import constants
 
 
-def stream_logs_from_generator(name: str, generator: Generator):
+def stream_logs_from_generator(name: str, generator: Generator, since_raw):
     asyncio.set_event_loop(asyncio.new_event_loop())
     http_client = HTTPClient()
     while True:
         try:
             line: bytes = generator.__next__()
             split_line: List[bytes] = line.split(b' ', maxsplit=1)
+            timestamp = split_line[0].decode('utf-8')
+            if since_raw and timestamp <= since_raw:
+                continue
             to_send: Dict[str, str] = {
-                'timestamp': split_line[0].decode('utf-8'),
+                'timestamp': timestamp,
                 'message': split_line[1].decode('utf-8'),
                 'nid': name
             }
